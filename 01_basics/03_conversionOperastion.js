58548e061060747677f1d687f33169413ebbd5aa
kajolSingh123 <69797443+kajolSingh123@users.noreply.github.com>
2024-01-29 19:09:55 +0000

    conversionOperastion

---
+++ 01_basics/03_conversionOperastion.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+console.log(typeof score);
+console.log (typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber );
+console.log (ValueInNumber);
+
+let isLoggedIn ="kajol"
+let booleanIsLoggedIn = Boolean( isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true 0=> false
+"" =>false
+"kajol" = true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof (stringNumber))
