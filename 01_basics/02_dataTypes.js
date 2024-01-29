bc299b387c103acc1afd97eb8caf9bfa64f949dc
kajolSingh123 <69797443+kajolSingh123@users.noreply.github.com>
2024-01-29 18:14:08 +0000

    DataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version.
+
+ // alert( 3+3 ) // we are using node js not browser
+
+/* Number => 12
+ BigInt
+ String = ""
+ Boolean=> true or false
+ null = standalone CSSMathValue
+ undefined=>
+ Symbol=> find unique value    
+ 
+ object
+
+ console.log(typeOf undefind) o/p  undefind
+ console.log(typeOf null)   o/p   object
+  
+ */
