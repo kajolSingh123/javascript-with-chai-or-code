5e67f3141812c2cdd851445a6f9392091873a4e5
kajolSingh123 <69797443+kajolSingh123@users.noreply.github.com>
2024-01-30 17:43:18 +0000

    numberAurMath

---
+++ 01_basics/06_numberAurMath.js
@@ -0,0 +1,11 @@
+const score = 400
+console.log (score);
+
+const balance = new Number (100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber =123.8992
+console.log(otherNumber.toPrecision(4));
+
