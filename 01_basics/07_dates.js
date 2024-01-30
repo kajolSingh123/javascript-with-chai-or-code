257976e2bfed24a2c2d2a01e679fb650770496b1
kajolSingh123 <69797443+kajolSingh123@users.noreply.github.com>
2024-01-30 18:02:41 +0000

    Dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toString());
+console.log( typeof myDate) // mydate is object.
